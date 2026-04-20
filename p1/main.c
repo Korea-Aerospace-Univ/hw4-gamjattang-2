2808059ea6b5018bda1bd16e16f69ae8e10c2734
2024125084 <kmj35003500@gmail.com>
2026-04-20 16:12:07 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -9,23 +9,20 @@ int main(void)
   
     scanf("%d", &target); //정답 입력
 
-    do
-        {
-        scanf("%d", &guess); //추측값 입력
-        count++;             //시도횟수증가
+   do {
+    scanf("%d", &guess);
+    count++;
 
-        if (guess > target)             //추측값이 목푯값보다 클 경우
-            printf("%d>?\n", target);   //힌트
-        
-        else if (guess < target)         //추측값이 목푯값보다 작을 경우
-            printf("%d<?\n", guess);              //비교
-              
-        
-        else                          //맞췄을경우
-            printf("%d==?\n", guess);       //정답과 비교한 값 출력
-           } while (guess != target);        //정답을 맞출때까지 반복
+    if (guess > target)
+        printf("%d>?\n", guess);        //추측값이 목푯값보다 클 경우
+    else if (guess < target)
+        printf("%d<?\n", guess);        //추측값이 목푯값보다 작을 경우
+    else
+        printf("%d==?\n", guess);       //맞췄을경우
 
-            printf("%d\n", count);   //총 시도 횟수 출력
+} while (guess != target);             //정답을 맞출때까지 반복
+
+printf("%d회\n", count);               //총 시도 횟수 출력
 
     return 0;
 }
